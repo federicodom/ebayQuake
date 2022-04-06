8defcdcad320738cf9f232ad6787b69da7b40096
Federico Dominguez <federicod.dominguez@gmail.com>
2022-04-06 12:41:12 -0300

    Add appColors and new sizes for appIcon

---
+++ eBayQuake/eBayQuake/Utilities/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  eBayQuake
+//
+//  Created by Federico Dominguez on 06/04/2022.
+//
+
+import UIKit
+
+enum Images {
+   // static let placeholder      = UIImage(named: "avatar-placeholder")
+}
+
+enum Colors {
+    static let tittle           = UIColor(named: "Tittle")
+    static let earthquakeRed        = UIColor(named: "EarthquakeRed")
+    static let earthquakeGreen        = UIColor(named: "EarthquakeGreen")
+    static let subtitle         = UIColor(named: "Subtitle")
+    static let secondTitleLabel = UIColor(named: "SecondTitleLabel")
+    static let datetimeLabel        = UIColor(named: "DatetimeLabel")
+}
